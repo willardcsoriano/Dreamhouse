2e9c6f3e1eee14dfdca51c8c7521d087df7e9003
willardcsoriano <soriano.willard@gmail.com>
2026-07-16 18:55:53 +0800

    Create and deploy housingMap Lightning Web Component

---
+++ force-app/main/default/lwc/housingMap/housingMap.js
@@ -0,0 +1,28 @@
+import { LightningElement, wire } from "lwc";
+import getHouses from "@salesforce/apex/HouseService.getRecords";
+
+export default class HousingMap extends LightningElement {
+    mapMarkers;
+    error;
+
+    @wire(getHouses)
+    wiredHouses({ error, data }) {
+        if (data) {
+            // Transform Apex response into lightning-map format
+            this.mapMarkers = data.map((element) => {
+                return {
+                    location: {
+                        Street: element.Address__c,
+                        City: element.City__c,
+                        State: element.State__c
+                    },
+                    title: element.Name
+                };
+            });
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.mapMarkers = undefined;
+        }
+    }
+}
